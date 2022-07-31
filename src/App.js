08aa8e10f0e5f2019ec4eae0abfc2518b66d3079
Luany Oliveira <luanyoliveira16@gmail.com>
2022-07-31 19:39:40 -0300

    início do projeto miniblog em react usando o firebase

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import './App.css';
+import React from 'react';
+import About from './pages/About/About';
+import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
+import Register from './pages/Register/Register';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+       <Navbar/>
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/about" element={<About />}></Route>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/registrar" element={<Register />}></Route>
+          </Routes>
+        </div>
+        <Footer/>
+      </BrowserRouter>
+    </div>
+  );
+}
+ 
+export default App;
+
+
+
---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firebase"
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDpyu8RrcOuMbwKPGzdNg7WATmUwEJsIvc",
+  authDomain: "miniblog-cf406.firebaseapp.com",
+  projectId: "miniblog-cf406",
+  storageBucket: "miniblog-cf406.appspot.com",
+  messagingSenderId: "975703210796",
+  appId: "1:975703210796:web:43aa4bfa42268542c50a89"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
